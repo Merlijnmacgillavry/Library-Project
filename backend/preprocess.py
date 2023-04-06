70c422c7764f0319089f302521ac55ccec3ff98e
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2023-04-06 23:36:34 +0200

    decoupled preprocessing from tfidf
    Now can do one time preprocessing seperate from whatever model we run

--- backend/preprocess.py
+++ backend/preprocess.py
@@ -1,18 +1,25 @@
 import logging
 import os
-
-from utils import CustomFormatter
+import pandas as pd
+import tqdm as td
+from utils import CustomFormatter, size_and_amount_files
 
 
 class PreProcess:
     def __init__(self, data_folder='data', dataframe_name='data.csv'):
         self.init_logger()
         self.logger.debug(f"Initializing {__class__.__name__}...")
+
         self.data_folder = data_folder
         self.dataframe_name = dataframe_name
         self.logger.info(f"Data folder: '{self.data_folder}'")
         self.logger.info(f"Dataframe name: '{self.dataframe_name}'")
+
         self.dataframe_exists = os.path.exists(dataframe_name)
+
+        self.file_size, self.amount_files = size_and_amount_files(
+            folder=data_folder)
+
         self.logger.info(f"Initialized {__class__.__name__}!")
         self.combine()
 
@@ -34,8 +41,23 @@ class PreProcess:
         if (not self.dataframe_exists):
             self.logger.warning(
                 f"Dataframe '{self.dataframe_name}' does not exist")
-            # TODO:
-            # Do actual combining like in tfidf
+            df = None
+
+            with td.tqdm(total=self.file_size, unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+                for file_name in os.listdir(self.data_folder):
+                    file_path = os.path.join(self.data_folder, file_name)
+                    try:
+                        new_df = pd.read_csv(file_path)
+                        if (df is None):
+                            df = new_df
+                        else:
+                            df = pd.concat([df, new_df])
+                    except Exception as e:
+                        self.logger.warning(f"[{file_path}] - {e}")
+                    pbar.update(os.path.getsize(file_path))
+
+            self.logger.info(f"Loaded csv: #{len(df)} rows.")
+            self.dataframe = df
             self.save_dataframe()
         else:
             self.logger.info(
--- backend/tfidf.py
+++ backend/tfidf.py
@@ -11,7 +11,7 @@ import pickle
 
 
 class TFIDF:
-    def __init__(self, data_folder='data', model_name='model.pkl', dataframe_name='dataframe.csv', records_per_page=20):
+    def __init__(self, data_folder='data', model_name='model.pkl', dataframe_name='data.csv', records_per_page=20):
         self.init_logger()
         self.logger.debug("Initializing TFIDF...")
         self.data_folder = data_folder
@@ -30,7 +30,7 @@ class TFIDF:
             f"Total data size is { convert_size(self.file_size)} , amount of files is {self.amount_files}")
 
         self.model_exists = os.path.exists(model_name)
-        self.dataframe_exists = os.path.exists(dataframe_name)
+        self.data_exists = os.path.exists(dataframe_name)
         self.model_matrix = None
         self.logger.info("Initialized TFIDF.")
         self.load_model()
@@ -49,12 +49,16 @@ class TFIDF:
 
     def load_model(self):
         self.logger.debug("Loading TFIDF model...")
+        if (not self.data_exists):
+            self.logger.error(
+                f"Failed to load data! - File '{self.dataframe_name}' does not exist!")
+            return
 
-        if ((not self.model_exists) or (not self.dataframe_exists)):
+        if ((not self.model_exists)):
             if not self.model_exists:
                 self.logger.warning(
                     f"Model '{self.model_name}' does not exist")
-            if not self.dataframe_exists:
+            if not self.data_exists:
                 self.logger.warning(
                     f"Dataframe '{self.dataframe_name}' does not exist")
             self.create_model()
@@ -69,35 +73,25 @@ class TFIDF:
             self.logger.debug(f"Loading dataframe '{self.dataframe_name}'!")
 
             self.dataframe = pd.read_csv(self.dataframe_name)
+
             self.logger.info("Loaded TFIDF dataframe.")
+
+        self.logger.debug(f"Loading model matrix...")
+        columns = ['title', 'author', 'research group', 'contributor', 'publication year',
+                   'abstract', 'subject topic', 'publication type', 'programme']
+        self.model_matrix = self.model.transform(self.dataframe[columns].apply(
+            lambda x: ' '.join(x.dropna().astype(str)), axis=1))
+        self.logger.info(f"Model matrix loaded!")
 
     def create_model(self):
         self.logger.debug(f"Creating model...")
-        self.logger.debug(f"Loading data...")
-        df = None
-
-        with td.tqdm(total=self.file_size, unit='B', unit_scale=True, unit_divisor=1024) as pbar:
-            for file_name in os.listdir(self.data_folder):
-                file_path = os.path.join(self.data_folder, file_name)
-                try:
-                    new_df = pd.read_csv(file_path)
-                    if (df is None):
-                        df = new_df
-                    else:
-                        df = pd.concat([df, new_df])
-                except Exception as e:
-                    self.logger.warning(f"[{file_path}] - {e}")
-                pbar.update(os.path.getsize(file_path))
-
-        self.logger.info(f"Loaded csv: #{len(df)} rows.")
-
-        self.logger.debug(f"Saving dataframe as '{self.dataframe_name}'")
 
         self.logger.debug(f"Initializing TFIDVectorizer...")
         tfidf = TfidfVectorizer(stop_words='english')
 
         columns = ['title', 'author', 'research group', 'contributor', 'publication year',
                    'abstract', 'subject topic', 'publication type', 'programme']
+
         self.logger.debug(f"Loading columns:\n{columns}")
 
         self.dataframe = df
@@ -117,27 +111,15 @@ class TFIDF:
             self.logger.error(
                 f"Failed to save model '{self.model_name}': {e}")
 
-    def save_dataframe(self):
-        self.logger.debug(f"Saving dataframe '{self.dataframe_name}'...")
-        try:
-            self.dataframe.to_csv(self.dataframe_name, index=False)
-            self.logger.info(
-                f"Saved dataframe '{self.dataframe_name}' successfully")
-        except Exception as e:
-            self.logger.warning(
-                f"Failed to save dataframe '{self.dataframe_name}': {e}")
-
     def search(self, query, page):
         start = (page - 1) * self.records_per_page
         end = start + self.records_per_page
-        if (self.model_matrix == None):
-            columns = ['title', 'author', 'research group', 'contributor', 'publication year',
-                       'abstract', 'subject topic', 'publication type', 'programme']
-            self.model_matrix = self.model.transform(self.dataframe[columns].apply(
-                lambda x: ' '.join(x.dropna().astype(str)), axis=1))
+
         query_tfidf = self.model.transform([query])
+
         cosine_similarities = cosine_similarity(
             query_tfidf, self.model_matrix).flatten()
+
         similarity_scores = sorted(
             list(enumerate(cosine_similarities)), key=lambda x: x[1], reverse=True)
 
@@ -154,4 +136,4 @@ class TFIDF:
 
 if __name__ == '__main__':
     tfidf = TFIDF()
-    print(tfidf.search("What is information retrieval?"))
+    # print(tfidf.search("What is information retrieval?", 12))
